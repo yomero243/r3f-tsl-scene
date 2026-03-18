d2573e874a4de1bb87adfe0c16736bbc6e250bdb
Garbriel Cerdio <yo_mero_yo@hotmail.com>
2026-03-18 10:42:05 -0600

    chore: configure Vite to keep esbuild names and set build target to esnext

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    keepNames: true,
+  },
+  build: {
+    target: 'esnext',
+  },
 })
